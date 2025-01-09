e9cedd601fb7f88c414dfd0bd2c5d39f9fa8eae7
LucasVTK <lucazim.vtk@gmail.com>
2025-01-09 12:28:47 -0300

    criando a seção de planos

--- src/main.js
+++ src/main.js
@@ -1,32 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    const buttons = document.querySelectorAll('[data-tab-button]')
+    const buttons = document.querySelectorAll('[data-tab-button]');
     
-
-    for(let i = 0; i < buttons.length; i++){
-        buttons[i].addEventListener('click', function(botao){
-            const abaAlvo = botao.target.dataset.tabButoon
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
-            escondeTodasAbas()
-            aba.classList.add('shows__list--is-active')
-            removeBotaoAtivo()
-            botao.target.classList.add('shows__tabs__button--is-active')
-        })
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton; // Corrigido: tabButoon -> tabButton
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active');
+        });
     }
-})
+});
 
-function removeBotaoAtivo(){
-    const buttons = document.querySelectorAll('[data-tab-button]')
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
 
-    for(let i = 0; i < buttons.length; i++){
-        buttons[i].classList.remove('shows__tabs__button--is-active')
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
 
-function escondeTodasAbas(){
-    const tabsContainer = document.querySelectorAll('[data-tab-id]')
+function escondeTodasAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
-    for(let i = 0; i < tabsContainer.length; i++){
-        tabsContainer[i].classList.remove('shows__list--is-active')
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
     }
 }
